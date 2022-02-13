3545180f4d7f5e8c5a009eb1a8f413491e61e9f8
Mahmud2640 <mdmahmud484@gmail.com>
2022-02-14 00:30:13 +0600

    add some code

--- js/banking-site.js
+++ js/banking-site.js
@@ -1,5 +1,20 @@
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
-    const depositInput = document.getElementById('')
+    const depositInput = document.getElementById("deposit-input");
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    // get current deposit
+    const depositTotal = document.getElementById("deposit-total");
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotalAmmount = parseFloat(depositTotalText);
+    depositTotal.innerText = previousDepositTotalAmmount + depositAmount;
+
+    const balanceTotal = document.getElementById("balance-total");
+
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
+    depositInput.value = "";
   });
